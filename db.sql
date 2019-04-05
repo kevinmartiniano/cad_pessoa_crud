d33a70e16d94021a0e9931d598df9dc4cc375112
kevinmartiniano <kevin.martiniano123@gmail.com>
2019-04-04 21:06:06 -0300

    comandos para criar banco e tabela

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db_cad_pessoa;
+USE db_cad_pessoa;
+
+CREATE TABLE pessoa (
+    id INT(12) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25) NOT NULL,
+    endereco VARCHAR(50) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    nascimento DATE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
